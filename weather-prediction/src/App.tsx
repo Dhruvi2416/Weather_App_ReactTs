43225a42a7017557083790e5c9c8cb5283635525
Dhruvi2416 <dhruvi.s@simformsolutions.com>
2023-05-15 18:36:36 +0530

    debouncing

--- weather-prediction/src/App.tsx
+++ weather-prediction/src/App.tsx
@@ -1,18 +1,18 @@
-import React from 'react'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import Home from './components/Home'
-import HourlyData from './components/HourlyData'
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./components/CurrentTemp";
+import HourlyData from "./components/HourlyData";
 const App = () => {
   return (
     <>
-    <Router>
-      <Routes>
-      <Route path='/' element={<Home/>}/>
-      <Route path='/hourly' element={<HourlyData/>}/>
-      </Routes>
-    </Router>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/hourly" element={<HourlyData />} />
+        </Routes>
+      </Router>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
---
+++ weather-prediction/src/components/CurrentTemp.tsx
@@ -0,0 +1,50 @@
+import React, { useEffect, useState, useMemo } from "react";
+import { debounce } from "lodash";
+import winterPic from "../winter.gif";
+const CurrentTemp = () => {
+  const [data, setData] = useState<any>(null);
+
+  const [city, setCity] = useState<string>("Nadiad");
+
+  const fetchData = async () => {
+    const response = await fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=cd2783566a4f7cfc5814d5f6dc152b6c&units=metric`
+    );
+    let datas = await response.json();
+
+    setData(() => datas);
+  };
+  const debouncedFetchData = debounce(fetchData, 300); // Create debounced function once
+
+  useEffect(() => {
+    debouncedFetchData();
+    return () => {
+      debouncedFetchData.cancel(); // Cancel any pending debounced invocation when the component unmounts
+    };
+  }, [city]);
+
+  return (
+    <div className="weather" style={{ backgroundImage: `url(${winterPic})` }}>
+      {data != null ? (
+        <>
+          {data.main != null && data.main.temp != null ? (
+            <p>{data.main.temp}</p>
+          ) : (
+            <p>No data found</p>
+          )}
+        </>
+      ) : (
+        <p>Loading...</p>
+      )}
+      <form>
+        <input
+          type="text"
+          placeholder="Search any city"
+          onChange={(e) => setCity(e.target.value)}
+        ></input>
+      </form>
+    </div>
+  );
+};
+
+export default CurrentTemp;
